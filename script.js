971b56e3e1a1eb067a064a42f8213fe3e6fd9f7b
mariaroncenmotta-eng <maria.roncen.motta@escola.pr.gov.br>
2026-06-12 14:26:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function mostrarMensagem() {
+}function mostrarMensagem() {
+    alert(
+        "A sustentabilidade no agronegócio garante produção eficiente e preservação ambiental."
+    );
+}
+
+let totalArvores = 0;
+
+function plantarArvore() {
+    totalArvores++;
+    document.getElementById("contador").textContent = totalArvores;
+}
+
+let totalArvores = 0;
+
+function plantarArvore() {
+    totalArvores++;
+    document.getElementById("contador").textContent = totalArvores;
+}
